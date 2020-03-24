fd0fcf60c41a9fdbab0ad95e9d00562f430c270d
Ravina <40683703+ravinaNG@users.noreply.github.com>
2020-03-24 17:14:08 +0530

    doing with function

--- demo.py
+++ demo.py
@@ -2,5 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.naukri.com/non-profit-jobs"
-request = requests.get(url)
-print (request)
+linkedIn = "https://in.linkedin.com/jobs/nonprofit-jobs?position=1&pageNum=0"
+
+def extract_source(url):
+    source = requests.get(linkedIn).content
+    return source
+
+print (extract_source(linkedIn))
