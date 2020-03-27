8406357b7f0b0e75ee3719ffaa991d6aa5157b4b
Ravina <40683703+ravinaNG@users.noreply.github.com>
2020-03-27 15:57:04 +0530

    ***

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # python librery to pull out data from 
+import html5lib # pure python library for parshing markup into html
+from pprint import pprint
 
 url = "https://www.naukri.com/non-profit-jobs"
 linkedIn = "https://in.linkedin.com/jobs/nonprofit-jobs?position=1&pageNum=0"
@@ -9,11 +11,11 @@ def extract_source(url):
     return source
 
 def extract_data(source):
-    soup = BeautifulSoup(source, html5lib)
-    names = soup.findAll('title')
-    return names
+    soup = BeautifulSoup(source, "html5lib")
+    # names = soup.findAll('title')
+    return soup
 
 source = extract_source(linkedIn)
 jobs = extract_data(source)
-print (jobs)
+pprint (jobs)
 
